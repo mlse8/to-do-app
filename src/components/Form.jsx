4af6b94b0efb57c37b199134738a0e738c58c377
Laura Escalante <laura088.le@gmail.com>
2024-03-25 22:04:00 -0300

    change form validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,38 +1,49 @@
-import { useState } from 'react';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { FormControl, FormLabel, TextField, Button } from '@mui/material';
 
-export default function Form({ addTask }) {
-    const [inputValue, setInputValue] = useState('');
+const validationSchema = Yup.object({
+    task: Yup.string()
+        .min(4, "Debe tener al menos 4 caracteres")
+        .max(100, "No puede tener más de 100 caracteres")
+        .required("Este campo es obligatorio"),
+});
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (inputValue.trim() !== '') {
-            addTask({ text: inputValue, completed: false });
-            setInputValue('');
-        }
-    };
-    
-    const handleInputChange = (e) => {
-        setInputValue(e.target.value);
+export default function FormFormik ({ addTask }) {
+    const handleSubmit = (values, { resetForm }) => {
+        addTask({ text: values.task, completed: false });
+        resetForm();
     };
 
     return (
-        <FormControl component="form" onSubmit={handleSubmit} fullWidth>
-            <FormLabel sx={{mb:2}}>
-                <TextField 
-                    fullWidth
-                    id="outlined-basic" 
-                    label="Ingrese la tarea" 
-                    variant="outlined" 
-                    color='secondary'
-                    value={inputValue} 
-                    onChange={handleInputChange}
-                />
-            </FormLabel>
-            <Button type="submit" variant="contained" sx={{mb:2}} color='secondary'>AGREGAR</Button>
-        </FormControl>
+        <Formik
+            initialValues={{ task: "" }}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+        >
+            {({ values, handleChange, handleBlur, errors, touched }) => (
+                <Form>
+                    <FormControl fullWidth>
+                        <FormLabel htmlFor="task" sx={{mb:2}}>
+                            <Field
+                                as={TextField}
+                                fullWidth
+                                id="task"
+                                name="task"
+                                variant="outlined"
+                                color="secondary"
+                                value={values.task}
+                                label="Ingrese la tarea"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                error={Boolean(errors.task && touched.task)}
+                                helperText={<ErrorMessage name="task" />}
+                            />
+                        </FormLabel>
+                        <Button type="submit" variant="contained" color="secondary" sx={{mb:2}}>AGREGAR</Button>
+                    </FormControl>
+                </Form>
+            )}
+        </Formik>
     );
-}
+};
